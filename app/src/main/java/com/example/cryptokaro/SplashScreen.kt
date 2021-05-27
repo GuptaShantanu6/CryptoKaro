d2a1826cd30beb5ade2c4f82605dc5370060727b
Shantanu Gupta <Gupta.Shantanu6@gmail.com>
2021-05-27 18:29:51 +0530

    SplashScreenAnim

--- app/src/main/java/com/example/cryptokaro/SplashScreen.kt
+++ app/src/main/java/com/example/cryptokaro/SplashScreen.kt
@@ -1,7 +1,9 @@
 package com.example.cryptokaro
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,6 +11,11 @@ class SplashScreen : AppCompatActivity() {
         setContentView(R.layout.activity_splash_screen)
 
         supportActionBar?.hide()
+
+        val b : View = findViewById(R.id.contBtn_splash_view)
+        b.setOnClickListener {
+            startActivity(Intent(this@SplashScreen,MainActivity::class.java))
+        }
 
 
 
