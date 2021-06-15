e149be97fa11490ff3fcfc723546c5673878a233
Shantanu Gupta <Gupta.Shantanu6@gmail.com>
2021-06-15 16:02:59 +0530

    fab reveal anim home activity

--- app/src/main/java/com/example/cryptokaro/MainActivity.kt
+++ app/src/main/java/com/example/cryptokaro/MainActivity.kt
@@ -1,18 +1,31 @@
 package com.example.cryptokaro
 
+import android.animation.Animator
 import android.content.Intent
+import android.content.res.ColorStateList
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewAnimationUtils
 import android.view.WindowManager
 import android.widget.Toast
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import com.example.cryptokaro.bottomNavFragments.HistoryFragmentBottomNav
 import com.example.cryptokaro.bottomNavFragments.HomeFragmentBottomNav
 import com.example.cryptokaro.bottomNavFragments.NotifFragmentBottomNav
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlin.math.hypot
+import kotlin.math.max
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mRevealLayout : View
+    private lateinit var mFab : FloatingActionButton
+
+    private var isRevealed = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -41,9 +54,109 @@ class MainActivity : AppCompatActivity() {
             true
         }
 
-        val fab : FloatingActionButton = findViewById(R.id.fabBtn)
-        fab.setOnClickListener {
-            startActivity(Intent(this@MainActivity, ProfileActivity::class.java))
+        mRevealLayout = findViewById(R.id.RevealLayout)
+
+        mFab = findViewById(R.id.fabBtn)
+        mFab.backgroundTintList = ColorStateList.valueOf(
+            ResourcesCompat.getColor(
+                resources,
+                R.color.appOrange,
+                null
+            )
+        )
+
+        mFab.setImageResource(R.drawable.ic_icons8_menu)
+
+        mFab.setOnClickListener {
+            revealLayoutFun()
+        }
+
+    }
+
+    private fun revealLayoutFun() {
+
+        if (!isRevealed) {
+
+            val x : Int = mRevealLayout.right
+            val y : Int = mRevealLayout.top
+
+            val startRadius = 0
+
+            val endRadius = hypot(
+                mRevealLayout.width.toDouble(),
+                mRevealLayout.height.toDouble()
+            ).toInt()
+
+            mFab.backgroundTintList = ColorStateList.valueOf(
+                ResourcesCompat.getColor(
+                    resources,
+                    R.color.appGrey,
+                    null
+                )
+            )
+
+            mFab.setImageResource(R.drawable.ic__close)
+
+            val anim = ViewAnimationUtils.createCircularReveal(
+                mRevealLayout,
+                x,
+                y,
+                startRadius.toFloat(),
+                endRadius.toFloat()
+            )
+
+            mRevealLayout.visibility = View.VISIBLE
+
+            anim.start()
+
+            isRevealed = true
+
+        }
+        else {
+
+            val x : Int = mRevealLayout.right
+            val y : Int = mRevealLayout.top
+
+            val startRadius : Int = max(mRevealLayout.width, mRevealLayout.height)
+
+            val endRadius = 0
+
+            mFab.backgroundTintList = ColorStateList.valueOf(
+                ResourcesCompat.getColor(
+                    resources,
+                    R.color.appOrange,
+                    null
+                )
+            )
+
+            mFab.setImageResource(R.drawable.ic_icons8_menu)
+
+            val anim = ViewAnimationUtils.createCircularReveal(
+                mRevealLayout,
+                x,
+                y,
+                startRadius.toFloat(),
+                endRadius.toFloat()
+            )
+
+            anim.addListener(object : Animator.AnimatorListener {
+
+                override fun onAnimationStart(animation: Animator?) {}
+
+                override fun onAnimationEnd(animation: Animator?) {
+                    mRevealLayout.visibility = View.GONE
+                }
+
+                override fun onAnimationCancel(animation: Animator?) {}
+
+                override fun onAnimationRepeat(animation: Animator?) {}
+
+            })
+
+            anim.start()
+
+            isRevealed = false
+
         }
 
     }
